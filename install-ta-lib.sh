157bbf92822aaa99ae7038dd97081dcf3add88b5
HarmonicReflux <bendoublebass@yahoo.de>
2023-12-24 05:54:32 +0000

    Set up shell script to download and compile binaries in order to make use of `pip install TA-Lib`. Modified docs.yml to make use of the shell scrip and decoupled installing TA-Lib from remaining 3rd party libraries.

---
+++ install-ta-lib.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Exit script if any command fails
+set -e
+
+# Update package list and install build tools
+sudo apt-get update
+sudo apt-get install -y build-essential wget
+
+# Download and install TA-Lib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+rm -rf ta-lib ta-lib-0.4.0-src.tar.gz
+
+# Set up the shared library path
+echo '/usr/local/lib' | sudo tee -a /etc/ld.so.conf.d/local.conf
+sudo ldconfig
+
+# Install TA-Lib using pip
+pip install TA-Lib
+
